3505aaebaf6883ed5711367002245da8e3fc7bcc
SerhiiZhygurov <sergwolyn228@gmail.com>
2021-01-04 20:52:48 +0200

    Added greetings from Serhii

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello from Cezary");
         System.out.println("Hello from Filip!");
+        System.out.println("Hello from Serhii!");
 
         System.out.println("Hello from Filip's branch");
     }
