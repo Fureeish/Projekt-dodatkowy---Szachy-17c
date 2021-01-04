3b568ed88c728211b94dcc369562ccd451a59194
Fureeish <f74.kwiatek@gmail.com>
2021-01-04 19:41:40 +0100

    Added greetings from Filip

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("hello from Cezary");
-  
-  }
+    public static void main(String[] args) {
+        System.out.println("hello from Cezary");
+        System.out.println("Hello from Filip!");
+    }
 }
