cc2b8f6caa67b20ab737cd225258f056f10dbc38
KajetanKarp <kajetankarp@gmail.com>
2021-01-04 19:54:38 +0100

    Added "Hello from Kajetan's branch!"

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello from Cezary");
         System.out.println("Hello from Filip!");
-
+        System.out.println("Hello from Kajetan's branch!");
         System.out.println("Hello from Filip's branch");
     }
 }
